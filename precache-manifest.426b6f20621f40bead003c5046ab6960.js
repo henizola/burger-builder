33f07540ad10af2882facfc015156331f74fbe68
henizola <z.w.henok@gmail.com>
2020-05-21 22:19:28 +0300

    Updates

---
+++ precache-manifest.426b6f20621f40bead003c5046ab6960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73055e489552a7e98db8ce7414e76f4c",
+    "url": "/henizola/burger-builder/index.html"
+  },
+  {
+    "revision": "cf4709e883af77e37162",
+    "url": "/henizola/burger-builder/static/css/main.bac21eaa.chunk.css"
+  },
+  {
+    "revision": "3a1b22303ee0b034b4d1",
+    "url": "/henizola/burger-builder/static/js/2.b0045bb7.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/henizola/burger-builder/static/js/2.b0045bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4709e883af77e37162",
+    "url": "/henizola/burger-builder/static/js/main.9315053c.chunk.js"
+  },
+  {
+    "revision": "8f8569972825f23d1312",
+    "url": "/henizola/burger-builder/static/js/runtime-main.f5a86dc3.js"
+  },
+  {
+    "revision": "c7fd13adf12b018ab6ced2549fe9fb42",
+    "url": "/henizola/burger-builder/static/media/1531643.c7fd13ad.svg"
+  }
+]);
